7a8e27b82d6dd4ab0762601d3e1bb2b2e8e2d211
Arya1206 <aryapatil1206@gmail.com>
2025-12-30 14:43:19 +0530

    commit1

---
+++ task1.py
@@ -0,0 +1,14 @@
+n1 = int (input ("Enter the first number: "))
+n2 = int (input ("Enter the second number: "))
+
+addition = n1 + n2
+print("Addition:",addition)
+
+subtraction = n1 - n2
+print("Subtraction:",subtraction)
+
+Multiplication = n1 * n2
+print("Multiplication:",Multiplication)
+
+Division = n1 / n2
+print("Division:",Division)
